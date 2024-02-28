2eda51a9ad972454cf96a8b5df1b549abfaf50ba
SamuelSirota <xsirotas@stuba.sk>
2024-02-28 10:27:13 +0100

    docs: updating docstrings

--- assignment2.py
+++ assignment2.py
@@ -1,9 +1,12 @@
-"""This module implements 'Who had breakfast first?' problem for the 
-assignment 01. 
+"""This module implements the dining savages problem for the 
+assignment 02. 
 
-Both Jano and Fero wake up, do morning hygiene,
-Jano eats first and then calls Fero.
-Only after Fero receives the call can he eat his breakfast."""
+All savages meet for a feast.
+Only when all of them are ready to eat can they start eating.
+They eat in a round-robin style.
+When the pot is empty, the cook is woken up to cook more food.
+When they are done eating, 
+they go back to the beginning and meet for another feast."""
 
 __authors__ = "Samuel Martin Sirota"
 __email__ = "xsirotas@stuba.sk"
@@ -21,14 +24,16 @@ class Barrier:
         self.mutex = Mutex()
         self.turnstile = Semaphore(0)
         
-    def wait(self, tid):
+    def wait(self, tid, string):
         """Waits for all threads to reach the barrier."""
         self.mutex.lock()
         self.counter += 1
-        print(f"Savage ({tid}) is waiting for others. ({self.counter}/{self.n})")
+        if string == "one by one":
+            print(f"Savage ({tid}) is waiting for others. ({self.counter}/{self.n})")
         if self.counter == self.n:
             self.counter = 0
-            print(f"Savage ({tid}) starts the feast.")
+            if string == "all at once":
+                print(f"Savage ({tid}) starts the feast.")
             self.turnstile.signal(self.n)
         self.mutex.unlock()
         self.turnstile.wait()
@@ -46,9 +51,16 @@ class Shared:
         
 
 def savage(shared, tid):
+    """This function simulates a savage.
+    It waits for all savages to be ready to eat.
+    Then it eats in a round-robin style.
+    When the pot is empty, it wakes up the cook.
+    Waits for the cook to fill the pot.
+    Eats again in a round-robin style.
+    After everyone is done eating, it goes back to the beginning."""
     while True:
-        shared.barrier1.wait(tid)
-        shared.barrier2.wait(tid)
+        shared.barrier1.wait(tid, "one by one")
+        shared.barrier2.wait(tid, "all at once")
         shared.mutex.lock()
         print(f"Savage ({tid}) is looking into the pot. ({shared.servings}) servings left.")
         if shared.servings == 0:
@@ -62,6 +74,9 @@ def savage(shared, tid):
         print(f"Savage ({tid}) is munching.")
         
 def cook(shared):
+    """This function simulates a cook.
+    It waits the pot to be empty.
+    Then it cooks the food and fills the pot."""
     while True:
         shared.emptyPot.wait()
         print("Cook is cooking.")
@@ -70,16 +85,16 @@ def cook(shared):
         print("Cook is putting the pot on the table.")
         shared.fullPot.signal()
     
+    
 def main():
-    """This function uses semaphores to solve the 'Who had breakfast
-    first' problem.
-    """
+    """This function initializes the shared data,
+    creates the thread for the savages and the cook"""
     n_threads = 7
     shared = Shared(n_threads)
     savages = [Thread(savage, shared, i) for i in range(n_threads)]
     cooks = Thread(cook, shared)
     [t.join() for t in savages]
-    cook.join()
+    cooks.join()
 
 
 if __name__ == "__main__":
